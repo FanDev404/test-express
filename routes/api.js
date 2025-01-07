01fbc0ac7a9b8bc5a7f2d551c6b170bd0c03c217
Fan Dev <fandev404@gmail.com>
2025-01-08 01:38:39 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,13 @@
+var express = require("express");
+var router = express.Router();
 
+router.get("/cekml", async (req, res, next) => {
+    res.json({
+	    status: true,
+	    data: {
+            developer: "irfan",
+            contact: "089674310267"
+        },
+	    msg: "Succecs",
+    })
+}
